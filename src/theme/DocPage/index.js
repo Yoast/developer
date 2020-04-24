6058945ad999a23d7dfae7c5ca1a150b75eb021b
Jimmy Comack <jimmy@yoast.com>
2020-04-24 13:30:36 +0200

    Added PoC for auto-prefixing the page title

--- src/theme/DocPage/index.js
+++ src/theme/DocPage/index.js
@@ -23,41 +23,86 @@ function matchingRouteExist( routes, pathname ) {
 	return routes.some( route => matchPath( pathname, route ) );
 }
 
+function getSanitizedLocation( location ) {
+	return location.pathname.replace( /\/$/, "" );
+}
+
+let parentNode = {};
+
+function getCurrentSidebarItem( items, targetItem ) {
+	return items.reduce( ( result, current ) => {
+		if ( current.type !== "category" ) {
+			if ( current.href !== "" && current.href === targetItem ) {
+				return { ...current, parent: parentNode };
+			}
+
+			return result;
+		}
+
+		parentNode = current;
+
+		if ( ! current.items || current.items.length === 0 ) {
+			return result;
+		}
+
+		let nItems = getCurrentSidebarItem( current.items, targetItem );
+
+		if ( nItems.label ) {
+			return nItems;
+		}
+
+		return result;
+	}, {} );
+}
+
+function getPageParent( sidebarEntries, location ) {
+	const match = getCurrentSidebarItem( sidebarEntries, location );
+
+	// Test for empty
+	if ( Object.keys( match ).length === 0 ) {
+		return "";
+	}
+
+	return match.parent.label;
+}
+
 function DocPage( props ) {
-	const {route, docsMetadata, location} = props;
-	const {permalinkToSidebar, docsSidebars, version} = docsMetadata;
-	const sidebar = permalinkToSidebar[location.pathname.replace( /\/$/, '' )] || 'mainSidebar';
-	const {siteConfig: {themeConfig = {}} = {}} = useDocusaurusContext();
-	const {sidebarCollapsible = true} = themeConfig;
+	const { route, docsMetadata, location } = props;
+	const { permalinkToSidebar, docsSidebars, version } = docsMetadata;
+	const { siteConfig: { themeConfig = {} } = {} } = useDocusaurusContext();
+	const { sidebarCollapsible = true } = themeConfig;
+	const sidebar = permalinkToSidebar[getSanitizedLocation( location )] || "mainSidebar";
 
 	let content = (
-		<MDXProvider components={MDXComponents}>
-			{renderRoutes( route.routes )}
+		<MDXProvider components={ MDXComponents }>
+			{ renderRoutes( route.routes ) }
 		</MDXProvider>
 	);
 
 	if ( !matchingRouteExist( route.routes, location.pathname ) ) {
 		content = (
-			<NotFound {...props} />
+			<NotFound { ...props } />
 		);
 	}
 
+	const parent = getPageParent( docsSidebars[sidebar], getSanitizedLocation( location ) );
+
 	return (
-		<Layout version={version}>
-			<div className={styles.docPage}>
-				<div className={styles.docSidebarContainer}>
+		<Layout version={ version } titlePrefix={parent}>
+			<div className={ styles.docPage }>
+				<div className={ styles.docSidebarContainer }>
 					<Logo/>
-					{sidebar && (
+					{ sidebar && (
 						<DocSidebar
-							docsSidebars={docsSidebars}
-							location={location}
-							sidebar={sidebar}
-							sidebarCollapsible={sidebarCollapsible}
+							docsSidebars={ docsSidebars }
+							location={ location }
+							sidebar={ sidebar }
+							sidebarCollapsible={ sidebarCollapsible }
 						/>
-					)}
+					) }
 				</div>
-				<main className={styles.docMainContainer}>
-					{content}
+				<main className={ styles.docMainContainer }>
+					{ content }
 				</main>
 			</div>
 		</Layout>

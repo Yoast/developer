48e3b3bfd3d50928a86e787ce4109f61ff799290
Jimmy Comack <jimmy@yoast.com>
2020-08-13 15:02:11 +0200

    Update to latest version

--- src/plugins/yoast-docs-assets/index.js
+++ src/plugins/yoast-docs-assets/index.js
@@ -2,22 +2,25 @@ const fs = require( "fs-extra" );
 const path = require( "path" );
 const CopyWebpackPlugin = require( "copy-webpack-plugin" );
 
+const defaultStrategy = {"module.plugins": "append"};
+
 module.exports = ( context, options ) => {
 	return {
 		name: "yoast-docs-assets",
 		configureWebpack( config, isServer, utils ) {
 			if ( !isServer ) {
-				return;
+				return { mergeStrategy: defaultStrategy };
 			}
 
 			const outDir = `${ context.outDir }`;
 			const staticDir = path.resolve( context.outDir, `../${ context.siteConfig.customFields.docsPath }/static/` );
 
 			if ( !fs.existsSync( staticDir ) ) {
-				return;
+				return { mergeStrategy: defaultStrategy };
 			}
 
 			return {
+				mergeStrategy: defaultStrategy,
 				plugins: config.plugins.concat( [
 					new CopyWebpackPlugin( [
 						{

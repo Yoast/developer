efe775639b9dfebffbea7f29cdcecc0b9f2fd9f0
Josee Wouters <info@joseewouters.nl>
2021-05-19 12:10:16 +0200

    Add metabox prio filter to sidebar

--- sidebars.js
+++ sidebars.js
@@ -156,6 +156,7 @@ module.exports = {
 						'customization/yoast-seo/changing-enhanced-slack-sharing',
 						'customization/yoast-seo/disabling-yoast-seo',
 						'customization/yoast-seo/filters/capability-roles-filter',
+						'customization/yoast-seo/filters/change-metabox-prio-filter',
 						'customization/yoast-seo/filters/cornerstone-post-types-filter',
 						'customization/yoast-seo/filters/markdown-enabled-filter',
 						'customization/yoast-seo/filters/primary-term-taxonomies-filter',

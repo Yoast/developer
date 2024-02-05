f10edbd6cb05ce9cfca020ca18610eddb85bbbef
petaryoast <86779744+petaryoast@users.noreply.github.com>
2024-02-05 10:51:11 +0100

    Resolve lint errors and reduce warnings

--- src/theme/BlogSidebar/index.js
+++ src/theme/BlogSidebar/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import {useWindowSize} from '@docusaurus/theme-common';
-import BlogSidebarDesktop from '@theme/BlogSidebar/Desktop';
-import BlogSidebarMobile from '@theme/BlogSidebar/Mobile';
-export default function BlogSidebar({sidebar}) {
-  const windowSize = useWindowSize();
-  if (!sidebar?.items.length) {
-    return null;
-  }
-  // Mobile sidebar doesn't need to be server-rendered
-  if (windowSize === 'mobile') {
-    return <BlogSidebarMobile sidebar={sidebar} />;
-  }
-  return <BlogSidebarDesktop sidebar={sidebar} />;
+import React from "react";
+import { useWindowSize } from "@docusaurus/theme-common";
+import BlogSidebarDesktop from "@theme/BlogSidebar/Desktop";
+import BlogSidebarMobile from "@theme/BlogSidebar/Mobile";
+
+/**
+ * Renders blog sidebar.
+ *
+ * @param {object} sidebar A sidebar.
+ * @returns {JSX.Element|null} A blog sidebar.
+ */
+export default function BlogSidebar( { sidebar } ) {
+	const windowSize = useWindowSize();
+	if ( ! sidebar?.items.length ) {
+		return null;
+	}
+	// Mobile sidebar doesn't need to be server-rendered
+	if ( windowSize === "mobile" ) {
+		return <BlogSidebarMobile sidebar={ sidebar } />;
+	}
+	return <BlogSidebarDesktop sidebar={ sidebar } />;
 }
